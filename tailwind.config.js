fe8880f220e67496eb83d232ae31c0f33a1049b1
ErickEduardo027 <es2022-3938@uce.edu.do>
2025-10-22 02:52:08 -0400

    Fase5: UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./src/**/*.{ts,tsx}"],
-  theme: { extend: {} },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3B82F6", // Azul principal
+        background: "#0a0a0a", // Fondo base
+        card: "#111111", // Color de tarjetas
+      },
+    },
+  },
   plugins: [],
-}
+};
+
